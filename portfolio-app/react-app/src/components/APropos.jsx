44890ad1aeaa12fd172277e1bcf0087f9c855136
AmadouBenRahimCAMARA <amadou.ben.rahim.camara@gmail.com>
2025-07-28 21:50:34 +0000

    Configurations mineures 3

--- portfolio-app/react-app/src/components/APropos.jsx
+++ portfolio-app/react-app/src/components/APropos.jsx
@@ -17,7 +17,7 @@ const About = () => {
         <img src="/photo.jpg" alt="RAHIM CAMARA" className="profile-picture" />
         <TypeAnimation
           sequence={[
-            'Développeur Full-Stack',
+            'Développeur Full-Stack en freelance',
             1500,
             'Passionné de technologies',
             1500,
@@ -30,7 +30,7 @@ const About = () => {
         />
         <p>
           Étudiant en fin de licence informatique, passionné par le développement web et mobile. 
-          Je suis constamment à la recherche de nouvelles technologies et de nouveaux défis pour améliorer mes compétences.
+          Je suis enthousiaste pour vous accompagner dans vos besoins de digitalisation et pour transformer vos idées en solutions numériques.
         </p>
       </motion.div>
     </section>
